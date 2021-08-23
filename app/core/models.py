d2cc1cd3a31fadcb76f385f5e7975e7e1823483c
SN Mirdamadi <snmirdamadi@gmail.com>
2021-08-23 08:06:01 -0700

    Super user is created and testing is implemented

--- app/core/models.py
+++ app/core/models.py
@@ -5,8 +5,19 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Creates and save a new user"""
+        if not email:
+            raise ValueError("User must have an email address")
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and save a super user"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -8,7 +8,7 @@ class ModelTests(TestCase):
         email = 'seyed@gmail.com'
         password = '@#$test'
         user = get_user_model().objects.create_user(
-            email=self.normalize_email(email),
+            email=email,
             password=password
         )
 
@@ -22,3 +22,13 @@ class ModelTests(TestCase):
         user = get_user_model().objects.create_user(email, 'test123')
 
         self.assertEqual(user.email, email.lower())
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@gmail.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
